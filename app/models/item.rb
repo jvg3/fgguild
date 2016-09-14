771f0adb2dde1bf3e250ce087550927edc1010ca
John Gregory <JohnG@johns-mbp-2.attlocal.net>
2016-09-14 13:14:35 -0500

    color items

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,4 +21,26 @@ class Item < ActiveRecord::Base
     :offHand]
   end
 
+  def self.color_for_ilvl(ilvl)
+
+    rbg = "rgba(0, 129 , 255,"
+    rbg = "rgba(198, 0 , 255," if ilvl >= 835
+    rbg = "rgba(255, 128, 0," if ilvl >= 850
+
+    # opacity = ((ilvl - 800)/35)*0.4 + 0.3
+    # opacity = ((ilvl - 835)/15)*0.4 + 0.3 if ilvl >= 835
+    # opacity = ((ilvl - 850))*0.4 + 0.3 if ilvl >= 850
+    # opacity = 0.3 if opacity < 0.3
+    # opacity = 0.7 if opacity > 0.7
+
+    # str = rbg + opacity.round(1).to_s + ")"
+    # puts "\n\n\nSTR"
+    # puts str
+    # puts "---\n\n\n"
+
+    return "background-color: " + rbg + ".5)"
+
+
+  end
+
 end
