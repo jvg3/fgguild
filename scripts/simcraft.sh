2aba84356e64cc66a652f537093d6e402bb8eb5f
John Gregory <jgregory@agari.com>
2018-08-22 10:13:24 -0700

    simc_current

--- scripts/simcraft.sh
+++ scripts/simcraft.sh
@@ -1,1 +1,1 @@
-./simc/engine/simc armory=us,kelthuzad,$1 calculate_scale_factors=1 json=reports/$1.json html=reports/$1.html iterations=400 report_details=0
+./simc/engine/simc armory=us,kelthuzad,$1 calculate_scale_factors=1 json=reports/$1.json html=./reports/$1.html iterations=4000 report_details=0
