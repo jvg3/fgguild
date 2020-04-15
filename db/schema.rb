7ce5262fbf768254bcdd1b1d2437db9f5248ed4c
jvg3 <john@bellwethermade.com>
2020-04-15 16:24:16 -0700

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,30 +15,30 @@ ActiveRecord::Schema.define(version: 20180821230201) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "items", force: :cascade do |t|
-    t.string   "name"
-    t.string   "slot"
-    t.integer  "member_id"
-    t.integer  "ilvl"
+  create_table "items", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "slot"
+    t.integer "member_id"
+    t.integer "ilvl"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "enchant"
-    t.integer  "gem0"
-    t.integer  "gem1"
-    t.integer  "gem2"
-    t.integer  "item_id"
-    t.integer  "bonus"
+    t.integer "enchant"
+    t.integer "gem0"
+    t.integer "gem1"
+    t.integer "gem2"
+    t.integer "item_id"
+    t.integer "bonus"
   end
 
-  create_table "members", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "class_id"
-    t.string   "role"
-    t.integer  "ilvl"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "dps"
-    t.text     "sim_results"
+  create_table "members", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.integer "class_id"
+    t.string "role"
+    t.integer "ilvl"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "dps"
+    t.text "sim_results"
   end
 
 end
